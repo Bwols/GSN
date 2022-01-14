332e36b9079b0268cea0682fa71bacbf4e34148c
Bartłomiej Wolski <rudy10003@gmail.com>
2022-01-14 10:47:12 +0100

    load_model in label_student fix

--- label_student.py
+++ label_student.py
@@ -6,6 +6,8 @@ import torchvision
 import torchvision.transforms as transforms
 imsize = 256
 loader = transforms.Compose([transforms.Scale(imsize), transforms.ToTensor()])
+import cv2 as cv
+
 
 transform = transforms.Compose(
             [
@@ -13,20 +15,24 @@ transform = transforms.Compose(
              transforms.Normalize((0,), (1,)),  # zakres 0,1
              ])
 
-def label_student():
-    directory = os.fsencode("models")
-    path_to_model = str(directory)[2:-1] + "\\" +  str(os.listdir(directory))[3:-2] 
+def label_student(path_to_model):
+    #directory = os.fsencode("models")
+    #path_to_model = str(directory)[2:-1] + "\\" +  str(os.listdir(directory))[3:-2]
 
-    model = load_model(path_to_model)
+    model = load_model(path_to_model,architecture="ResNet50",image_size=64)
     model = model.cuda()
     model.eval()
 
-    image = image_loader("Pokemon_Images_Student/1.png")
-    model(image)
+    image = image_loader("Pokemon_Images/1.png")
+    print(image.shape[:])
+    x = model(image)
+    print(x)
+
+
 def image_loader(image_name):
     """load image, returns cuda tensor"""
     image = Image.open(image_name)
     image = transform(image).to('cuda').unsqueeze(0)
     return image
 
-label_student()
+label_student("models/run_0_ResNet50.ckpt")
