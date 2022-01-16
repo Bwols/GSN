c86c916c66f918ece9adcd986c0787d56c7daf48
krzysztofMajchrzak-GIT <58195641+krzysztofMajchrzak-GIT@users.noreply.github.com>
2022-01-16 20:30:39 +0100

    Update working on collab

--- preperSets.py
+++ preperSets.py
@@ -9,26 +9,27 @@ STUDENTFOLDER = "PokemonDataStudent"
 
 def preperFolders():
 
-    shutil.copytree(SOURCEFOLDER, TEACHERFOLDER)
-    shutil.copytree(SOURCEFOLDER, STUDENTFOLDER)
+    if not os.path.isdir(TEACHERFOLDER):
+        shutil.copytree(SOURCEFOLDER, TEACHERFOLDER)
+    else:
+      return
+    if not os.path.isdir(STUDENTFOLDER):   
+        shutil.copytree(SOURCEFOLDER, STUDENTFOLDER)
+    else:
+      return
 
-    directory = os.fsencode(STUDENTFOLDER)
+    directory = STUDENTFOLDER
     
     for folder in os.listdir(directory):
         #foldername = os.fsdecode(folder)[2:-1]
-        subDirectory = os.fsencode(STUDENTFOLDER +"\\" + folder)
+        subDirectory = STUDENTFOLDER +"/" + str(folder)
         iterator = 0
         for file in os.listdir(subDirectory):
-            fileString = str(file)[2:]
-            fileString = fileString[:-1]
-            os.remove(STUDENTFOLDER +"\\" + foldername + "\\" + fileString)
+            fileString = str(file)
+            os.remove(STUDENTFOLDER +"/" + str(folder) + "/" + fileString)
             iterator = iterator + 1
             if iterator == 5:
                 break
         for file in os.listdir(subDirectory):
-            fileString = str(file)[2:]
-            fileString = fileString[:-1]
-            os.remove(TEACHERFOLDER +"\\" + foldername + "\\" + fileString)
-            
-
-
+            fileString = str(file)
+            os.remove(TEACHERFOLDER +"/" + str(folder) + "/" + fileString)
